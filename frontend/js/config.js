cfdfc411b3fe5cc2d91c094e1eb12f9a00beb7a1
Kulchart Tunyuwattana <kulchart.t@gmail.com>
2026-04-14 19:20:37 +0700

    ignore config.js

--- frontend/js/config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-  API_URL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-    ? 'http://localhost:3000'
-    : 'https://khai-claude-production.up.railway.app'
-};
